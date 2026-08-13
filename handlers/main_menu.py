a63c3f98a5de480e1652dc0aa9992c1c8c0430be
zov6275-create <zov6275@gmail.com>
2026-08-13 10:48:09 +0500

    Update main_menu.py

--- handlers/main_menu.py
+++ handlers/main_menu.py
@@ -22,7 +22,9 @@ async def main_menu(message: types.Message):
     """
     person_id = message.from_user.id
     new_sql.user_in_bd(person_id)
-    await message.answer(
+    await message.answer_photo(
+    photo="8965415545",
+    caption=
     f'👋 {hbold("Добро пожаловать!")}\n\n'
     f'💼 {hbold("Надёжный сервис для безопасных сделок!")}\n'
     f'✨ Автоматизировано, быстро и без лишних хлопот!\n\n'
